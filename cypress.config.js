4e8b449d7eb3b467e6b48eb9550dab4b6db9754f
humnamursalin <humnamursalin17@gmail.com>
2025-11-30 22:32:25 -0800

    add cypress config

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8081',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    video: false,
+    screenshotOnRunFailure: true,
+  },
+})
--- frontend/cypress/cypress.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    e2e: {
-      baseUrl: "http://localhost:8080"
-    }
-  }
-  
