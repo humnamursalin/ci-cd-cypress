e93cfc096cbede0efa76c7ca4909ecec4c4d4858
humnamursalin <humnamursalin17@gmail.com>
2025-11-30 22:47:26 -0800

    remove support file

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -8,7 +8,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
     video: false,
     screenshotOnRunFailure: true,
   },
