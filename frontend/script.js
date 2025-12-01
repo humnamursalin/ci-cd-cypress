3f6a3ff19f9f805667f4e9b6b09c5fc700bde788
humnamursalin <humnamursalin17@gmail.com>
2025-11-30 23:45:31 -0800

    failing test

--- frontend/script.js
+++ frontend/script.js
@@ -6,6 +6,4 @@ document.getElementById("change-color").onclick = () => {
     document.getElementById("main-title").style.color = "blue";
 };
 
-document.getElementById("show-message").onclick = () => {
-    document.getElementById("message-box").innerText = "Button clicked!";
-};
+
